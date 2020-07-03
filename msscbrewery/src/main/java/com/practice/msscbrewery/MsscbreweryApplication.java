b03563ff8a4a9012bb6262cb0d955c87c462c3ce
suhaspartha <geekystark@gmail.com>
2020-07-03 09:51:41 +0530

    initial prohect commit

---
+++ msscbrewery/src/main/java/com/practice/msscbrewery/MsscbreweryApplication.java
@@ -0,0 +1,13 @@
+package com.practice.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscbreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscbreweryApplication.class, args);
+	}
+
+}
---
+++ msscbrewery/src/test/java/com/practice/msscbrewery/MsscbreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscbreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
